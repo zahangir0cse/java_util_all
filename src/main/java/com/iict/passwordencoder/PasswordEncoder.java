0ffef7135c2fe1ddf4efe8fe9dec87c883eb404c
Zahangir Alam <zahangir0cse@gmail.com>
2019-12-31 11:59:31 +0600

    first commit

---
+++ src/main/java/com/iict/passwordencoder/PasswordEncoder.java
@@ -0,0 +1,12 @@
+package com.iict.passwordencoder;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoder {
+    public static void main(String[] args) {
+        String password = "1234";
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode(password);
+        System.out.println(hashedPassword);
+    }
+}
