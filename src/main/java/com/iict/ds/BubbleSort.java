5b0c08c1a894f8e90c46aad1ccdb01fe51110a7f
Zahangir <zahangir0cse@gmail.com>
2020-12-06 18:49:58 +0600

    add bubble sort

---
+++ src/main/java/com/iict/ds/BubbleSort.java
@@ -0,0 +1,24 @@
+package com.iict.ds;
+
+public class BubbleSort {
+    public static int[] bubbleSort(int[] elements) {
+        int numberOfElements = elements.length;
+        int temp;
+        for (int i = 0; i < numberOfElements; i++) {
+            for (int j = 0; j < numberOfElements - i - 1; j++) {
+                if (elements[j] > elements[j + 1]) {
+                    temp = elements[j];
+                    elements[j] = elements[j + 1];
+                    elements[j + 1] = temp;
+                }
+            }
+        }
+        return elements;
+    }
+
+    public static void main(String[] args) {
+        int[] elements = {10, 2, 3, 5, 1, 12, 7};
+        int[] bubbleSortOfElements = bubbleSort(elements);
+        BinarySearch.printElements("Sorted Elements by Bubble Sort: ", bubbleSortOfElements);
+    }
+}
