416f4c4e207b09ea1d914147ad350d647aa2bcea
Zahangir Alam <zahangir0cse@gmail.com>
2021-02-09 11:37:16 +0600

    solve leetcode problems

---
+++ src/main/java/com/iict/leetcode/ThreeSum.java
@@ -0,0 +1,50 @@
+package com.iict.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> listOfIntList = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return listOfIntList;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int total = nums[i] + nums[left] + nums[right];
+                if (total < 0) {
+                    left++;
+                } else if (total > 0) {
+                    right--;
+                } else {
+                    List<Integer> intList = new ArrayList<>();
+                    intList.add(nums[i]);
+                    intList.add(nums[left]);
+                    intList.add(nums[right]);
+                    listOfIntList.add(intList);
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return listOfIntList;
+    }
+}
