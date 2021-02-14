f1bd63f9f166dfd829e729660e0ea18a014bcfbb
Zahangir Alam <zahangir0cse@gmail.com>
2021-02-15 02:07:53 +0600

    solve leetcode problems

--- src/main/java/com/iict/leetcode/AddTwoNumbers.java
+++ src/main/java/com/iict/leetcode/AddTwoNumbers.java
@@ -6,8 +6,34 @@ public class AddTwoNumbers {
         ListNode l2 = new ListNode(3, new ListNode(4, new ListNode(10, new ListNode(11))));
         AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
         ListNode newListNode = addTwoNumbers.addTwoNumbers(l1, l2);
+        ListNode listNode = mergeTwoLists(l1, l2);
 
     }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode margeNode = head;
+        ListNode first = l1;
+        ListNode second = l2;
+        while(first != null && second != null){
+            if(first.val < second.val){
+                margeNode.next = first;
+                first = first.next;
+            }else{
+                margeNode.next = second;
+                second = second.next;
+            }
+            margeNode = margeNode.next;
+        }
+        if(first != null){
+            margeNode.next = first;
+        }
+        if(second != null){
+            margeNode.next = second;
+        }
+        return head.next;
+    }
+
     public ListNode addTwoNumbers(ListNode p, ListNode q) {
         ListNode dummyHead = new ListNode(0);
         ListNode curr = dummyHead;
