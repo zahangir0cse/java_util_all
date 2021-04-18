d5e862f35e1e452c6dd5491cfba830a68ea1d325
Zahangir Alam <zahangir0cse@gmail.com>
2021-04-19 01:40:20 +0600

    add Synchronization in java

---
+++ src/main/java/com/iict/concurrency/SyncronizationMain.java
@@ -0,0 +1,41 @@
+package com.iict.concurrency;
+
+class Counter{
+    int num = 100;
+
+    public synchronized void increase(){
+        num++;
+    }
+}
+
+public class SyncronizationMain {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+//        counter.increase();
+//        counter.increase();
+//        counter.increase();
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    counter.increase();//1000
+                }
+
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {//2000
+                    counter.increase();
+                }
+
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(counter.num);
+    }
+}
