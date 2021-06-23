e932dc3a58cdea01e0e30320af5c81a21fd0a089
Zahangir Alam <zahangir0cse@gmail.com>
2021-06-23 16:16:08 +0600

    add mail utility

--- src/main/java/com/iict/mail/MailConstraint.java
+++ src/main/java/com/iict/mail/MailConstraint.java
@@ -14,7 +14,7 @@ public final class MailConstraint {
     public static final String PORT = "25";
     public static final String HOST = "mail.crackcodebd.com";
     public static final String IS_AUTH = "true";
-//    public static final String IS_TLS = "true";
+    public static final String IS_TLS = "true";
 
     public static Properties getPropertiesInstance() {
         Properties properties = new Properties();
@@ -22,8 +22,10 @@ public final class MailConstraint {
         properties.put("mail.smtp.port", PORT);
         properties.put("mail.smtp.auth", IS_AUTH);
         properties.put("mail.debug", "true");
-//        properties.put("mail.smtp.starttls.enable", IS_TLS);
-        properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        properties.put("mail.smtp.starttls.enable", IS_TLS);
+        properties.put("mail.smtp.tls.enable", IS_TLS);
+        properties.put("mail.smtp.ssl.trust","*");
+//        properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
         return properties;
     }
 
--- src/main/java/com/iict/passwordencoder/PasswordEncoder.java
+++ src/main/java/com/iict/passwordencoder/PasswordEncoder.java
@@ -4,7 +4,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordEncoder {
     public static void main(String[] args) {
-        String password = "1234Zaha";
+        String password = "123456";
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String hashedPassword = passwordEncoder.encode(password);
         System.out.println(hashedPassword);
