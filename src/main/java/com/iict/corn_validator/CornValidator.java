61d81c179090b794e0f9c9689fbc54a6a0008f8f
Zahangir Alam <zahangir0cse@gmail.com>
2022-03-14 14:19:30 +0600

    london time zone

--- src/main/java/com/iict/corn_validator/CornValidator.java
+++ src/main/java/com/iict/corn_validator/CornValidator.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class CornValidator {
     public static void main(String[] args) {
-        CronSequenceGenerator cronSequenceGenerator = new CronSequenceGenerator("0 0 3 1 * *");
+        CronSequenceGenerator cronSequenceGenerator = new CronSequenceGenerator("0 0 0 * * ?");
         System.out.println(cronSequenceGenerator.next(new Date()));
         double d = Double.valueOf(13)/2;
         System.out.println(d);//6.5
--- src/main/java/com/iict/date/DateUtils.java
+++ src/main/java/com/iict/date/DateUtils.java
@@ -1,11 +1,41 @@
 package com.iict.date;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class DateUtils {
+    private static final String LONDON_TIME_ZONE = "Europe/London";
     public static void main(String[] args) throws ParseException {
-        System.out.println(new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS").parse("2021-06-20T08:54:00.000"));
+//        System.out.println(getCurrentZonedDate(LONDON_TIME_ZONE));
+
+
+        TimeZone tz = TimeZone.getTimeZone("Europe/London");
+
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss a");
+        df.setTimeZone(tz);
+
+        Date now = new Date();
+        String nowStr = df.format(now);
+        System.out.println(new SimpleDateFormat("dd/MM/yyyy hh:mm:ss a").parse(nowStr));
+        System.out.println(nowStr);
+    }
+
+    public static Date getCurrentZonedDate(String zoneName){
+//        String systemLocalDateTimeString = LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE_FORMAT_TIME));
+        LocalDateTime systemLocalDateTime = LocalDateTime.now();
+        ZoneId zoneId = ZoneId.of(zoneName);
+        ZonedDateTime zonedDateTime = systemLocalDateTime.atZone(zoneId);
+//        LocalDateTime localDateTime = LocalDateTime.from(zonedDateTime);
+        String dateFormat = "dd/MM/yyyy hh:mm:ss a";
+        System.out.println(new SimpleDateFormat(dateFormat).format(Date.from(zonedDateTime.toInstant())));
+        return Date.from(zonedDateTime.toInstant());
+
     }
 }
